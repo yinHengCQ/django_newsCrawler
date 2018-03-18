0994cdfb13490d8116c8e628cb21853953c6ab1a
yinhengcq <yinhengcq@163.com>
2018-03-18 12:39:59 +0800

    修复BUG，使用redis缓存爬虫数据

--- crawler/admin.py
+++ crawler/admin.py
@@ -8,10 +8,10 @@ class newsAdmin(admin.ModelAdmin):
 class proxyIpAdmin(admin.ModelAdmin):
     list_display = ['id','ip_address','ip_port','address','ip_type','source']
 class job51Admin(admin.ModelAdmin):
-    list_display = ['id','job_name','job_url','company_name','company_url','job_address','job_salary',
+    list_display = ['id','job_id','job_name','job_url','company_name','company_url','job_address','job_salary',
                     'pub_date','salary_low','salary_high','createTime','modifyTime']
 class job51DetailAdmin(admin.ModelAdmin):
-    list_display = ['id','job_name','company_name','company_desc','job_jtag','job_welfare','job_detail_desc',
+    list_display = ['id','job_id','job_name','company_name','company_desc','job_jtag','job_welfare','job_detail_desc',
                     'job_type_desc','job_keyword_desc','work_address','createTime','modifyTime']
 
 
--- crawler/scrapy_crawler/scrapy_crawler/spiders/Job51Spider.py
+++ crawler/scrapy_crawler/scrapy_crawler/spiders/Job51Spider.py
@@ -26,38 +26,47 @@ class Job51Spider(scrapy.Spider):
         log.msg("current page is {0}".format(response.url), level=log.INFO)
 
         for var in base_xpath:
-            item=Job51CrawlerItem()
-            item['job_id'] = var.xpath('p[1]/input[1]/@value').extract()[0]
-            item['job_name'] = var.xpath('p[1]/span[1]/a[1]/@title').extract()[0]
-            item['job_url'] = var.xpath('p[1]/span[1]/a[1]/@href').extract()[0]
-            detail_urls.append(item['job_url'])
-            item['company_name'] = var.xpath('span[@class="t2"][1]/a[1]/@title').extract()[0]
-            item['company_url'] = var.xpath('span[@class="t2"][1]/a[1]/@href').extract()[0]
-            item['job_address'] = var.xpath('span[@class="t3"][1]/text()').extract()[0]
-            item['job_salary'] = var.xpath('span[@class="t4"][1]/text()').extract()[0]
-            item['pub_date'] = var.xpath('span[@class="t5"][1]/text()').extract()[0]
-            salary_temp = salary_unicode2int(item['job_salary'])
-            if salary_temp == None:salary_low = 0;salary_high = 0
-            else:salary_low = salary_temp.get('low');salary_high = salary_temp.get('high')
-            item['salary_low']=salary_low
-            item['salary_high']=salary_high
-
-            print('*' * 50)
-            print(item['job_name'])
-            yield scrapy.Request(item['job_url'],meta={'item':item},callback=self.parse_detail)
+            try:
+                item = Job51CrawlerItem()
+                item['job_id'] = var.xpath('p[1]/input[1]/@value').extract()[0]
+                item['job_name'] = var.xpath('p[1]/span[1]/a[1]/@title').extract()[0]
+                item['job_url'] = var.xpath('p[1]/span[1]/a[1]/@href').extract()[0]
+                detail_urls.append(item['job_url'])
+                item['company_name'] = var.xpath('span[@class="t2"][1]/a[1]/@title').extract()[0]
+                item['company_url'] = var.xpath('span[@class="t2"][1]/a[1]/@href').extract()[0]
+                try:item['job_address'] = var.xpath('span[@class="t3"][1]/text()').extract()[0]
+                except:item['job_address']=''
+                try:item['job_salary'] = var.xpath('span[@class="t4"][1]/text()').extract()[0]
+                except:item['job_salary']=''
+                item['pub_date'] = var.xpath('span[@class="t5"][1]/text()').extract()[0]
+                salary_temp = salary_unicode2int(item['job_salary'])
+                if salary_temp == None:salary_low = 0;salary_high = 0
+                else:salary_low = salary_temp.get('low');salary_high = salary_temp.get('high')
+                item['salary_low']=salary_low
+                item['salary_high']=salary_high
+                yield scrapy.Request(item['job_url'], meta={'item': item}, callback=self.parse_detail)
+            except:pass
 
 
     def parse_detail(self, response):
         site = Selector(response)
         item=response.meta['item']
 
-        item['work_address'] = site.xpath('//div[@class="bmsg inbox"][1]/p[1]')[0].xpath('string(.)').extract()[0].strip()
-        item['company_desc']=site.xpath('//p[@class="msg ltype"][1]/text()').extract()[0].strip()
-        item['job_jtag']=''.join((var.xpath('text()').extract()[0]+"|") for var in site.xpath('//span[@class="sp4"]'))[:-1]
-        item['job_welfare']=''.join((var.xpath('text()').extract()[0]+"|") for var in site.xpath('//p[@class="t2"]/span'))[:-1]
-        temp=site.xpath('//div[@class="bmsg job_msg inbox"][1]')[0].xpath('string(.)').extract()[0].strip()
-        item['job_detail_desc']=temp[:temp.find(u'职能类别：')].strip()
-        item['job_type_desc']=''.join((var.xpath('text()').extract()[0]+'|') for var in site.xpath('//div[@class="mt10"][1]/p[@class="fp"][1]/span[@class="el"]'))
-        item['job_keyword_desc']=''.join((var.xpath('text()').extract()[0]+'|') for var in site.xpath('//div[@class="mt10"][1]/p[@class="fp"][2]/span[@class="el"]'))
+        try:item['work_address'] = site.xpath('//div[@class="bmsg inbox"][1]/p[1]')[0].xpath('string(.)').extract()[0].strip()
+        except:item['work_address']=''
+        try:item['company_desc']=site.xpath('//p[@class="msg ltype"][1]/text()').extract()[0].strip()
+        except:item['company_desc']=''
+        try:item['job_jtag']=''.join((var.xpath('text()').extract()[0]+"|") for var in site.xpath('//span[@class="sp4"]'))[:-1]
+        except:item['job_jtag']=''
+        try:item['job_welfare']=''.join((var.xpath('text()').extract()[0]+"|") for var in site.xpath('//p[@class="t2"]/span'))[:-1]
+        except:item['job_welfare']=''
+        try:
+            temp = site.xpath('//div[@class="bmsg job_msg inbox"][1]')[0].xpath('string(.)').extract()[0].strip()
+            item['job_detail_desc'] = temp[:temp.find(u'职能类别：')].strip()
+        except:item['job_detail_desc']=''
+        try:item['job_type_desc']=''.join((var.xpath('text()').extract()[0]+'|') for var in site.xpath('//div[@class="mt10"][1]/p[@class="fp"][1]/span[@class="el"]'))
+        except:item['job_type_desc']=''
+        try:item['job_keyword_desc']=''.join((var.xpath('text()').extract()[0]+'|') for var in site.xpath('//div[@class="mt10"][1]/p[@class="fp"][2]/span[@class="el"]'))
+        except:item['job_keyword_desc']=''
 
         yield item
